afc6e3947c5cfe419c9a9cbb07115cbc8eb8c11a
xvMari <kozinkaa53@gmail.com>
2026-03-31 11:42:26 +0200

    Test3

--- src/main/java/org/example/Kalkulator.java
+++ src/main/java/org/example/Kalkulator.java
@@ -1,4 +1,26 @@
 package org.example;
 
 public class Kalkulator {
+    public static double podziel(int a, int b){
+//        nie bedzie b = 0
+
+
+        return a /(double) b;
+    }
+    public static boolean czyLiczbaPiewsza(int liczba){
+//        TODO
+        if(liczba == 1){
+            return false;
+        }
+        double pierwiastek = Math.sqrt(liczba);
+        for (int i = 2; i <= pierwiastek; i++) {
+            if (liczba % i == 0){
+                return false;
+            }
+
+        }
+
+        return true;
+
+    }
 }
--- src/test/java/org/example/KalkulatorTest.java
+++ src/test/java/org/example/KalkulatorTest.java
@@ -1,4 +1,44 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class KalkulatorTest {
-  
+
+    @Test
+    void testPodzielJezeliCalkowity() {
+        Assertions.assertEquals(3, Kalkulator.podziel(6, 2));
+    }
+    @Test
+    void testPodzielJezeliReczywisty() {
+        Assertions.assertEquals(3.5, Kalkulator.podziel(7, 2));
+    }
+
+
+    @Test
+    void testczyLiczbaPiewsza() {
+        Assertions.assertFalse(Kalkulator.czyLiczbaPiewsza(1));
+    }
+    @Test
+    void testczyLiczbaPiewszaParzysta() {
+        Assertions.assertTrue(Kalkulator.czyLiczbaPiewsza(2));
+    }
+    @Test
+    void testczyLiczbaPiewszaParzystaZlozona() {
+        Assertions.assertFalse(Kalkulator.czyLiczbaPiewsza(6));
+    }
+    @Test
+    void testczyLiczbaPiewszaNieparzysta() {
+        Assertions.assertTrue( Kalkulator.czyLiczbaPiewsza(17));
+    }
+    @Test
+    void testczyLiczbaPiewszaKwadratPierwszej() {
+        Assertions.assertFalse(Kalkulator.czyLiczbaPiewsza(49));
+    }
+    @Test
+    void testczyLiczbaPiewszaDuza() {
+        Assertions.assertTrue(Kalkulator.czyLiczbaPiewsza(1117));
+    }
 }
