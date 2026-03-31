ba4535cd60201bc4827a56a3c298090acc548c9a
xvMari <kozinkaa53@gmail.com>
2026-03-31 10:43:50 +0200

    Test

--- src/main/java/org/example/Szyf.java
+++ src/main/java/org/example/Szyf.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Szyf {
 
-    public String getSaszywrowane() {
+    public static String szyfrujCezarem(String slowoSzyfrowania, int klucz ){
         String saszywrowane = "";
         return saszywrowane;
     }
--- src/test/java/org/example/SzyfTest.java
+++ src/test/java/org/example/SzyfTest.java
@@ -1,11 +1,31 @@
 package org.example;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 class SzyfTest {
 
     @Test
-    void getSaszywrowane() {
+    void testszyfrujCezaremJezeliKluczDodatni() {
+        Assertions.assertEquals("def", Szyf.szyfrujCezarem("abc", 3));
+    }
+
+    @Test
+    void testszyfrujCezaremJezeliZawijane(){
+        Assertions.assertEquals("abc", Szyf.szyfrujCezarem("def", 3));
+    }
+
+    @Test
+    void testszyfrujCezaremOdszyfrowanie() {
+        Assertions.assertEquals("abc", Szyf.szyfrujCezarem("xyz", 3));
+    }
+    @Test
+    void testszyfrujCezaremKluczDluzszyOdAlfabetu() {
+        Assertions.assertEquals("def", Szyf.szyfrujCezarem("abc", 29));
+    }
+    @Test
+    void testszyfrujCezaremSpacjaWTekscie() {
+        Assertions.assertEquals("cd ef", Szyf.szyfrujCezarem("ab cd", 2));
     }
 }
